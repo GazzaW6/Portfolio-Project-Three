2ca7ae1da0d5a9b1e5b8a016c7d03cfd8a80bfaf
GazzaW6 <garrettwhyte@gmail.com>
2023-01-14 11:20:23 +0000

    new code started for new game.

--- run.py
+++ run.py
@@ -1,21 +1,6 @@
+import random 
 
+print('Welcome to Hangman')
+print('--------------------------')
 
-# Board for holding players ship locations
-PLAYER_BOARD = [[" "] * 8 for x in range(8)]
-# Board for displaying hits and misses of computers board
-COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
-
-letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
-
-def board():
-    pass
-
-def create_ships():
-    pass
-
-def get_ship_location():
-    pass
-
-def count_hit_ships():
-    pass
-
+word_dictionary = ['sunflower', 'cars', 'robot' 'north', 'Europe', 'balloon', 'window']
