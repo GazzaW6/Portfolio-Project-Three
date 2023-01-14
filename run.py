1cc42877411dccb818d1c28613cb140035a23cb6
GazzaW6 <garrettwhyte@gmail.com>
2023-01-14 13:50:18 +0000

    add hangman image coding

--- run.py
+++ run.py
@@ -3,4 +3,54 @@ import random
 print('Welcome to Hangman')
 print('--------------------------')
 
-word_dictionary = ['sunflower', 'cars', 'robot' 'north', 'Europe', 'balloon', 'window']
+wordBank = ['sunflower', 'cars', 'robot' 'north', 'Europe', 'balloon', 'window']
+
+
+randomWord = random.choice(wordBank)
+
+for x in randomWord:
+    print("_", end=" ")
+
+def print_hangman(wrong):
+    if(wrong == 0):
+        print("\n+---+")
+        print("    |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 1): 
+        print("\n+---+")
+        print("O   |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 2):
+        print("\n+---+")
+        print("O   |")
+        print("|   |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 3):
+        print("\n+---+")
+        print(" O  |")
+        print("/|  |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 4):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 5):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("/   |")
+        print("   ===")
+    elif(wrong == 6):
+        print("\n+---+")
+        print(" O   |")
+        print("/|\  |")
+        print("/ \  |")
+        print("    ===")
