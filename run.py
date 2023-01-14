049624e148dff09616fd1cb9656d696cc2739060
GazzaW6 <garrettwhyte@gmail.com>
2023-01-14 10:26:00 +0000

    add initial function names for game

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+# Board for holding players ship locations
+PLAYER_BOARD = [[" "] * 8 for x in range(8)]
+# Board for displaying hits and misses of computers board
+COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
+
+def board():
+    pass
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
+
